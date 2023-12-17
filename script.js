e505b7638f20846f3b96c829cd54ab1ae162d903
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2023-12-17 23:54:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function isSameType(value1, value2) {
   //your js code here
+	if(value1[0] != '"' && value1[0] != "'"){
+	value1 = parseInt(value1);	
+	}
+
+	if(value2[0] != '"' && value2[0] != "'"){
+	value2 = parseInt(value2);	
+	}
+	
 	return  typeof value1 === typeof value2;
 }
-
-// do not change the code below.    
+  
+// do not change the code below.     
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
+// alert( value1); alert( value2);
 alert(isSameType(value1, value2));
